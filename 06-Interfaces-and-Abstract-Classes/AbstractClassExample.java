7113d3ce0d82dfa1d1efd9bf320f776c37b8976f
Altpugg <dirikahmet12@gmail.com>
2026-04-02 13:06:04 +0300

    Add 06-Interfaces-and-Abstract-Classes/AbstractClassExample.java

---
+++ 06-Interfaces-and-Abstract-Classes/AbstractClassExample.java
@@ -0,0 +1,21 @@
+abstract class Animal {
+	abstract void makeSound();
+	public void sleep() {
+		System.out.println("I am sleeping");
+	}
+}
+
+class Dog extends Animal {
+	public void makeSound() {
+		System.out.println("Bark");
+	}
+}
+
+public class AbstractClassExample {
+	public static void main(String[] args) {
+	
+Dog dog = new Dog();
+		dog.makeSound();
+		dog.sleep();
+	}
+}
